62a075afe68cd336043f9bca4e293ef84ca93f8f
Ali Salihi <salihiali111@gmail.com>
2025-11-10 17:54:54 +0100

    small change

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 bg-black text-white shadow-md z-50">
+      <div className="px-6 py-4">
+        <h1 className="text-2xl font-bold">Expense Tracker App - Inspire 11</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
