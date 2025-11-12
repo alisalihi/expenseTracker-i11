8c334bfb61c238d2d6d53df2752a1a954c8806db
Ali Salihi <salihiali111@gmail.com>
2025-11-12 13:13:46 +0100

    Task 6: Implement responsive sidebar toggle with mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./components/Home/Home";
 import "./App.css";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
   return (
     <div className="h-screen flex flex-col">
-      <Header />
+      <Header onMenuClick={toggleSidebar} />
 
       <div className="flex flex-1 pt-16">
-        <Sidebar />
+        <Sidebar isOpen={sidebarOpen} onClose={closeSidebar} />
 
-        <main className="flex-1 ml-64 overflow-y-auto bg-gray-50">
+        <main className="flex-1 lg:ml-64 overflow-y-auto bg-gray-50">
           <div className="p-6">
             <Home />
           </div>
